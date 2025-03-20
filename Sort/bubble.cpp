75472856c29d8db9f103fb3ed9a956e089185074
dhanush-shetty1 <dhanush.shetty@somaiya.edu>
2025-03-21 01:21:11 +0530

    sort

--- Sort/bubble.cpp
+++ Sort/bubble.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 using namespace std;
 
+int bubble(int arr[], int n){
+    for(int i =0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    cout<<"Sorted Array: ";
+    for(int i =0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
 int main(){
     int n;
     cout<<"Enter n:";
     cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"Enter element "<<i+1<<": ";
+        cin>>arr[i];
+    }
+    bubble(arr,n);
 
 }
